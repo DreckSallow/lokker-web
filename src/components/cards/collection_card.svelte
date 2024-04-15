89c5713202f29a97f0baa92398f74f50efad787a
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-04-15 18:16:48 -0500

    add collection card info and show some cards

---
+++ src/components/cards/collection_card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let tag: 'div' | 'li';
+	export let authorImg: string;
+	export let alt: string;
+	export let authorName: string;
+	export let title: string;
+	export let link: string;
+	// export let linkText: string;
+</script>
+
+<svelte:element this="{tag}" class="rounded-xl p-4 border border-neutral-300">
+	<header class="flex gap-4 items-center mb-2">
+		<img src="{authorImg}" alt="{alt}" class="rounded-full h-10 w-10" />
+		<h5 class="text-base text-brand/70 font-light">{authorName}</h5>
+	</header>
+	<h4 class="font-semibold text-xl text-brand mb-4">{title}</h4>
+	<a href="{link}" class="btn-solid inline-block text-sm px-3 py-2">Ver Colleccion</a>
+</svelte:element>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,31 @@
-<main class="px-6 py-2">
+<script lang="ts">
+	import CollectionCard from '../components/cards/collection_card.svelte';
+
+	let collections = [
+		{
+			authorImg: 'https://fakeimg.pl/200/',
+			alt: 'Avatar',
+			authorName: 'Dreck Sallow',
+			title: 'Rust ecosystem and more :)',
+			link: '/collection/1'
+		},
+		{
+			authorImg: 'https://fakeimg.pl/200/',
+			alt: 'Avatar',
+			authorName: 'Dreck Sallow',
+			title: 'Rust ecosystem and more :)',
+			link: '/collection/1'
+		}
+	];
+</script>
+
+<main class="px-10 py-2">
 	<header>
+		<h1 class="font-bold text-4xl mb-2">Search Collections</h1>
+		<h3 class="text-neutral-600 font-normal text-lg mb-6">Find the perfect read</h3>
 		<div
-			class="border-2 border-brand-soft rounded-xl flex justify-between overflow-hidden pr-3 w-max"
+			class="border border-neutral-400 rounded-xl flex justify-between overflow-hidden pl-3 w-[24rem]"
 		>
-			<input
-				type="text"
-				class="outline-none flex-1 px-4 py-3 text-brand placeholder:text-slate-400"
-				placeholder="Buscar collecciones"
-				spellcheck="false"
-			/>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
@@ -18,7 +35,27 @@
 					d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"
 				></path>
 			</svg>
+			<input
+				type="text"
+				class="outline-none flex-1 px-4 py-3 text-brand placeholder:text-slate-400"
+				placeholder="Buscar collecciones..."
+				spellcheck="false"
+			/>
 		</div>
 	</header>
-	<section></section>
+	<!--<hr class="h-[1px] bg-neutral-400 mt-4" />-->
+	<section class="mt-8">
+		<ul class="mx-auto grid grid-cols-3 gap-6 justify-start">
+			{#each collections as coll}
+			<CollectionCard
+				tag="li"
+				authorImg="{coll.authorImg}"
+				alt="{coll.alt}"
+				authorName="{coll.authorName}"
+				link="{coll.link}"
+				title="{coll.title}"
+			/>
+			{/each}
+		</ul>
+	</section>
 </main>
