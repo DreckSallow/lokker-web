efdfb785a40ffcea780c8a29309ffef7f31158cc
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-04-15 18:21:27 -0500

    style: add some ui changes

--- src/components/cards/collection_card.svelte
+++ src/components/cards/collection_card.svelte
@@ -10,9 +10,9 @@
 
 <svelte:element this="{tag}" class="rounded-xl p-4 border border-neutral-300">
 	<header class="flex gap-4 items-center mb-2">
-		<img src="{authorImg}" alt="{alt}" class="rounded-full h-10 w-10" />
-		<h5 class="text-base text-brand/70 font-light">{authorName}</h5>
+		<img src="{authorImg}" alt="{alt}" class="rounded-full h-9 w-9" />
+		<h5 class="text-base text-neutral-800 font-light">{authorName}</h5>
 	</header>
-	<h4 class="font-semibold text-xl text-brand mb-4">{title}</h4>
+	<h4 class="font-semibold text-xl text-brand mb-6">{title}</h4>
 	<a href="{link}" class="btn-solid inline-block text-sm px-3 py-2">Ver Colleccion</a>
 </svelte:element>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
-				class="w-6 h-[80%] fill-slate-700 my-auto"
+				class="w-6 h-[80%] fill-neutral-500 my-auto"
 			>
 				<path
 					d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"
@@ -37,7 +37,7 @@
 			</svg>
 			<input
 				type="text"
-				class="outline-none flex-1 px-4 py-3 text-brand placeholder:text-slate-400"
+				class="outline-none flex-1 px-4 py-3 text-brand placeholder:text-neutral-400"
 				placeholder="Buscar collecciones..."
 				spellcheck="false"
 			/>
