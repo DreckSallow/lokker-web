75bc9379841c163bb29cb70e5f36269a5f0d646d
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-04-22 23:11:14 -0500

    feat(comp): add avatar component

---
+++ src/components/imgs/avatar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let src: string | undefined = undefined;
+	export let alt: string;
+	export let abr = '';
+	export let imgClass = '';
+	export let divClass = '';
+	export let hClass = ''; // A height for class
+</script>
+
+{#if src}
+<img src="{src}" alt="{alt}" class="{`ui-circle ${hClass} ${imgClass}`}" loading="lazy" />
+{:else}
+<div class="{`ui-circle flex-center ${hClass} ${divClass}`}">{abr}</div>
+{/if}
