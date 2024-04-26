ca691d19704ca0fb75fc11f1d22bc09a592641f5
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-04-25 21:41:34 -0500

    style: add border to nav

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,6 +1,6 @@
 <script lang="ts"></script>
-<nav class="flex align-center justify-between py-4 px-6">
-	<div>
+<nav class="flex align-center justify-between py-4 px-6 border-b border-soft-brand">
+	<div class="flex justify-center items-center">
 		<a class="text-brand italic font-semibold text-xl" href="/">Lokker</a>
 	</div>
 	<ul class="flex gap-4">
