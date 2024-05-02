7142fbb04a3d02b4820a8a3a67f7cf396f96636a
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-05-01 21:48:11 -0500

    refactor: remove unused quotes

--- src/routes/profiles/[profile_id]/+page.svelte
+++ src/routes/profiles/[profile_id]/+page.svelte
@@ -10,7 +10,7 @@
 	<aside class="sticky top-0 p-6 flex flex-col items-center max-w-[250px] w-[250px]">
 		<Avatar
 			src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__2IIAULCR-xberpmuxf-9Jx3cJZLJgLm4tSb9cDwRQ&s"
-			abr="{data.profile.username.trim().at(0)}"
+			abr={data.profile.username.trim().at(0)}
 			alt="user profile"
 			hClass="h-[180px]"
 			divClass="text-5xl font-bold"
@@ -20,31 +20,31 @@
 		<p class="mt-3 text-black">{data.profile.bio}</p>
 		<ul class="flex flex-row gap-6 mt-6">
 			<li>
-				<a href="{data.profile.website_url}" target="_blank">
+				<a href={data.profile.website_url} target="_blank">
 					<SocialIcon name="web" styleClass="h-7 w-7" />
 				</a>
 			</li>
 			<li>
-				<a href="{data.profile.twitter_url}" target="_blank">
+				<a href={data.profile.twitter_url} target="_blank">
 					<SocialIcon name="twitter" styleClass="h-7 w-7" />
 				</a>
 			</li>
 		</ul>
 	</aside>
 	<section class="p-8 flex-1">
-		<h2 class="text-3xl font-bold mb-3">Collections</h2>
+		<h2 class="text-3xl font-bold mb-6">Collections</h2>
 		<ul class="grid grid-cols-3 gap-6">
 			{#each data.collections as coll}
-			<li
-				class="p-4 rounded-xl border border-neutral-400 flex flex-col justify-between items-start"
-			>
-				<h4 class="font-semibold text-xl mb-3">{coll.name}</h4>
-				<a
-					href="{`/collections/${coll.collection_id}`}"
-					class="rounded-lg bg-neutral-700 text-white text-sm px-3.5 py-1 inline-block"
-					>See collection</a
+				<li
+					class="p-4 rounded-xl border border-neutral-400 flex flex-col justify-between items-start"
 				>
-			</li>
+					<h4 class="font-semibold text-xl mb-3">{coll.name}</h4>
+					<a
+						href={`/collections/${coll.collection_id}`}
+						class="rounded-lg bg-neutral-700 text-white text-sm px-3.5 py-1 inline-block"
+						>See collection</a
+					>
+				</li>
 			{/each}
 		</ul>
 	</section>
