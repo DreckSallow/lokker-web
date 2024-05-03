e6fb21f8d07a6ffde6582868b4c8f17ab97f0993
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-05-02 22:38:22 -0500

    add card component

---
+++ src/lib/ui/components/card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let className = '';
+	export { className as class };
+	export let tag: 'div' | 'li' = 'div';
+</script>
+
+<svelte:element this={tag} class={`${className} p-6 bg-overpage rounded-xl shadow-md`}>
+	<slot></slot>
+</svelte:element>
