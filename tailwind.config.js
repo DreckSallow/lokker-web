f5c9c6770b57ff53fe73734b67e483010ac87025
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-05-02 22:37:37 -0500

    style: add more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
+        "page": "#fafafa", 
+        "overpage": "#ffffff", 
+        "subtext":"#666666",
+        "headtext":"#171717",
         "brand": "#141d26",
         "brand-soft": "#e0e4ea"
       }
