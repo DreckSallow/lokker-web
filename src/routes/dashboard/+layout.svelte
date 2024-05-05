9d86b22ee6cf2cc4f7cdb8d30de305719e99de36
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-05-04 22:38:54 -0500

    style: highlight route mismatch

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import type { LayoutData } from './$types';
 	import { setContext } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { Newspaper, User } from 'lucide-svelte';
+	import { page } from '$app/stores';
+	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 	const user = writable(null);
@@ -12,18 +13,26 @@
 </script>
 
 <main class="flex flex-row">
-	<aside class="w-[13rem] p-4 border-r-2 border-neutral-300/60 h-screen">
+	<aside class="w-[13rem] p-4 border-r-2 border-neutral-300/60 h-screen sticky top-0">
 		<a href="/" class="font-bold text-2xl italic text-center mb-6 block">Lokker</a>
 		<nav class="sidebar">
 			<ul class="flex flex-col gap-1">
-				<li class="hover:bg-neutral-200 p-2 rounded-lg">
-					<a href="/dashboard" class="flex gap-2 items-center font-semibold">
+				<li>
+					<a
+						href="/dashboard"
+						class="flex gap-2 items-center font-semibold hover:bg-neutral-100 p-2 rounded-lg"
+						class:bg-neutral-200={$page.url.pathname == '/dashboard'}
+					>
 						<User class="w-6 h-6" />
 						My Profile
 					</a>
 				</li>
-				<li class="hover:bg-neutral-200 p-2 rounded-lg">
-					<a href="/dashboard/collections" class="flex gap-2 items-center font-semibold">
+				<li>
+					<a
+						href="/dashboard/collections"
+						class="flex gap-2 items-center font-semibold hover:bg-neutral-100 p-2 rounded-lg"
+						class:bg-neutral-200={$page.url.pathname == '/dashboard/collections'}
+					>
 						<Newspaper class="w-6 h-6" />
 						My Collections
 					</a>
