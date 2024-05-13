323edb35fb39c09e933fb7f672ec3be7d8fdc0ea
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-05-13 16:51:54 -0500

    fix: add sinc prop in markdown editor

--- src/lib/ui/components/markdown-editor.svelte
+++ src/lib/ui/components/markdown-editor.svelte
@@ -6,34 +6,35 @@
 
 	let element: Element | undefined;
 	let editor: Editor | undefined;
+
 	onMount(() => {
 		editor = new Editor({
 			element: element,
-			extensions: [StarterKit],
+			extensions: [StarterKit.configure({ heading: { levels: [2, 3, 4, 5, 6] } })],
 			content,
 			autofocus: true,
 			onTransaction: () => {
 				editor = editor;
+			},
+			onUpdate({ editor }) {
+				content = editor.isEmpty ? '' : editor.getHTML();
 			}
 		});
 		element?.setAttribute('spellcheck', 'false');
 	});
+
 	onDestroy(() => {
 		if (editor) editor.destroy();
 	});
 </script>
 
-<div
-	bind:this={element}
-	class="w-full h-screen [&>.tiptap]:border-neutral-200/70 [&>.tiptap]:border prose prose-neutral prose-lg"
-/>
+<div bind:this={element} class="min-h-32 prose prose-neutral prose-lg w-full max-w-full" />
 
 <style>
 	div :global(.tiptap) {
 		height: 100%;
+		width: 100%;
 		outline: none;
-		border-width: 2px;
-		border-radius: 5px;
-		padding: 0.8rem;
+		min-height: inherit;
 	}
 </style>
