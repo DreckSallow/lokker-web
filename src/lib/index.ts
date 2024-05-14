7f0838ec68b35d6ee0797362c03f0b6fd5985c5d
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-05-14 17:35:23 -0500

    refactor: move the getDate parsed into lib file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,34 @@
 // place files you want to import through the `$lib` alias in this folder.
 
+export function getDate(timestamp: string) {
+  const date = new Date(timestamp);
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  const monthName = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${monthName} ${day}, ${year}`;
+}
+
+
+export function debounce(fn: Function, delay = 1000) {
+  let timeoutId: number | undefined;
+  return function() {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(fn, delay)
+  }
+}
--- src/routes/collections/[id]/+page.svelte
+++ src/routes/collections/[id]/+page.svelte
@@ -1,29 +1,7 @@
 <script lang="ts">
 	import AppNav from '../../../components/nav.svelte';
 	import type { PageData } from '../$types';
-	function getDate(timestamp: string) {
-		const date = new Date(timestamp);
-		const months = [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December'
-		];
-
-		const monthName = months[date.getMonth()];
-		const day = date.getDate();
-		const year = date.getFullYear();
-
-		return `${monthName} ${day}, ${year}`;
-	}
+	import { getDate } from '$lib';
 
 	export let data: PageData;
 
