6a6f84e42cfa779fd14f66c4d85948b451169ded
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-05-22 21:55:57 -0500

    feat: improve the collection ui/logic page

--- src/routes/collections/[id]/+page.server.ts
+++ src/routes/collections/[id]/+page.server.ts
@@ -1,14 +1,22 @@
+import type { PageLoad } from "./$types";
+import type { Article, Collection, UserProfile } from "$lib/types";
 import { api } from "$lib/utils/api";
-import type { PageLoad } from "../../$types";
+
+type ResponseType = {
+  collection: Collection & { user_id: number };
+  user_info: Pick<UserProfile, "username" | "email" | "profile_id">;
+  articles: Pick<Article, "article_id" | "title" | "update_at">[]
+}
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  async function get_info() {
+  async function get_info(): Promise<ResponseType> {
     const res = await fetch(api("/collections/", params.id));
     return await res.json();
   }
-  const { collection, articles } = await get_info();
+  const { collection, articles, user_info } = await get_info();
   return {
     collection,
-    articles
+    articles,
+    user_info
   }
 }
--- src/routes/collections/[id]/+page.svelte
+++ src/routes/collections/[id]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import AppNav from '../../../components/nav.svelte';
-	import type { PageData } from '../$types';
+	import type { PageData } from './$types';
 	import { getDate } from '$lib';
+	import { ChevronRight } from 'lucide-svelte';
+	import Avatar from '../../../components/imgs/avatar.svelte';
 
 	export let data: PageData;
 
@@ -54,32 +56,65 @@
 </script>
 
 <AppNav />
-<main class="flex gap-6 mx-10 pt-1 mt-6">
-	<nav class="w-[14rem] p-4 mt-10">
-		<ul class="border border-neutral-300 rounded-xl p-4">
-			{#each data.articles as article}
-				<li class="text-sm">
-					<button
-						on:click={() => getContent(article.article_id)}
-						class={`p-2 rounded-xl ${currentInfo.article?.article_id == article.article_id ? 'bg-neutral-100/80' : 'hover:underline'}`}
-					>
-						{article.title}
-					</button>
-				</li>
-			{/each}
-		</ul>
-	</nav>
-	<section class="flex-1">
-		{#if currentInfo.isLoading}
-			<span>Loading...</span>
-		{:else if currentInfo.article}
-			<h1 class="text-3xl font-bold mb-4 text-center">{currentInfo.article?.title}</h1>
-			<span class="ml-auto text-neutral-600 text-sm mb-4 block w-max">
-				{getDate(currentInfo.article.update_at)}
-			</span>
-			<div class="prose prose-neutral prose-lg w-full">{@html currentInfo.article?.content}</div>
-		{:else if currentInfo.error}
-			<p>Error: {currentInfo.error}</p>
-		{/if}
-	</section>
+<main class="py-4 px-14 flex flex-col gap-2">
+	<header class="flex flex-col gap-2 mt-2 mb-4 font-semibold items-start">
+		<div class="flex flex-row gap-2 items-center">
+			<a href="/" class="underline hover:text-brand/80 underline-offset-4"> Search collections </a>
+			<ChevronRight class="h-4 w-4" />
+			<span class="text-neutral-600">{data.collection.name}</span>
+		</div>
+	</header>
+	<div class="flex gap-16 mt-2">
+		<aside class="w-[14rem] mt-8 flex flex-col gap-4 justify-between">
+			<div>
+				<h5 class="border-b border-neutral-300 text-neutral-600 text-sm font-semibold mb-4">
+					Articles ({data.articles.length})
+				</h5>
+				<ul class="h-[250px] max-h-[250px]">
+					{#each data.articles as article}
+						<li class="text-sm">
+							<button
+								on:click={() => getContent(article.article_id)}
+								class={`p-2 rounded-lg text-start w-full ${currentInfo.article?.article_id == article.article_id ? 'bg-neutral-100/80' : 'hover:underline'}`}
+							>
+								{article.title}
+							</button>
+						</li>
+					{/each}
+				</ul>
+			</div>
+			<div>
+				<h5 class="border-b border-neutral-300 text-neutral-600 text-sm font-semibold mb-4">
+					Author
+				</h5>
+				<div class="flex gap-4 items-center">
+					<Avatar
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__2IIAULCR-xberpmuxf-9Jx3cJZLJgLm4tSb9cDwRQ&s"
+						abr={data.user_info.username.at(0)}
+						alt="profile"
+						hClass="h-[50px]"
+						divClass="text-2xl font-bold"
+					/>
+					<a href="/profiles/8" class="text-sm">
+						<h4 class="font-semibold">{data.user_info.username}</h4>
+						<span class="text-neutral-600 font-light">{data.user_info.email}</span>
+					</a>
+				</div>
+			</div>
+		</aside>
+		<section class="flex-1">
+			{#if currentInfo.isLoading}
+				<span class="text-3xl">Loading Content...</span>
+			{:else if currentInfo.article}
+				<h1 class="text-4xl font-bold mb-2 text-start">{currentInfo.article?.title}</h1>
+				<span class="ml-auto text-neutral-600 text-sm mb-2 block w-max">
+					Last update:
+					{getDate(currentInfo.article.update_at)}
+				</span>
+				<div class="prose prose-neutral prose-lg w-full">{@html currentInfo.article?.content}</div>
+			{:else if currentInfo.error}
+				<p>Error: {currentInfo.error}</p>
+			{/if}
+		</section>
+	</div>
 </main>
